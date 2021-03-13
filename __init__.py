99d0201b5f5b5ef3b55b2d9213ac76dea7e0417c
Thomas Harrison <twh2898@vt.edu>
2021-03-12 23:33:03 -0500

    Added verticalscrollbar.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 """A collection of utilities for tkinter and ttk.""" 
 
-__all__ = ['tk', 'ttk', 'PathControl']
+__all__ = ['tk', 'ttk', 'PathControl', 'VerticalScrolledFrame']
 
 import tkinter as tk
 import tkinter.ttk as ttk
 
 from .pathcontrol import PathControl
+from .verticalscrollbar import VerticalScrolledFrame
 
