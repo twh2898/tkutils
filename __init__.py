e37cca3ab41bdd2da74be5b25fa405170e40b839
Thomas Harrison <twh2898@vt.edu>
2021-03-12 23:20:17 -0500

    Added tk and ttk to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 """A collection of utilities for tkinter and ttk.""" 
 
-__all__ = ['PathControl']
+__all__ = ['tk', 'ttk', 'PathControl']
+
+import tkinter as tk
+import tkinter.ttk as ttk
 
 from .pathcontrol import PathControl
 
