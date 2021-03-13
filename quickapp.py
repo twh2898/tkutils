2bd6342a2c071d0e7ec9f9f6cb644fe0aecd5332
Thomas Harrison <twh2898@vt.edu>
2021-03-12 23:42:14 -0500

    Added title to quickapp

--- quickapp.py
+++ quickapp.py
@@ -2,8 +2,10 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
-def quickapp(cls):
+
+def quickapp(cls, title='tk'):
     root = tk.Tk()
+    root.title(title)
     cls(root).pack(expand=True, fill=tk.BOTH)
     root.update()
     root.minsize(root.winfo_width(), root.winfo_height())
